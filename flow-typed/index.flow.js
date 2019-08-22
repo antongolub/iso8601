94098a1b64796331f1716dd3d693d69b5fdb5c94
Anton Golub <mailbox@antongolub.ru>
2019-08-22 18:06:08 +0400

    feat: add TS libdefs
    closes #25

---
+++ flow-typed/index.flow.js
@@ -0,0 +1,5 @@
+// @flow
+
+declare module '@antongolub/iso8601' {
+  declare module.exports: (value: string, group?: string, date?: Date) => Date | void
+}
---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+declare module '@antongolub/iso8601' {
+  export default function (value: string, group?: string, date?: Date): Date | void;
+}
+
